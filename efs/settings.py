98317649f7c3ce56f888daf24eaf382330622d02
infiniteshi <wenyushi@unomaha.edu>
2018-02-25 21:20:48 -0600

    2/25/2018 commit: Investment modified

--- efs/settings.py
+++ efs/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's$yfdt9#-ies%$3%gt#mb*t*zzobjtx)8wn7u(1jzh((h6(*wd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- portfolio/views.py
+++ portfolio/views.py
@@ -100,7 +100,44 @@ def investment_list(request):
    investments = Investment.objects.filter(acquired_date__lte=timezone.now())
    return render(request, 'portfolio/investment_list.html', {'investments': investments})
 
+@login_required
+def investment_edit(request, pk):
+   investment = get_object_or_404(Investment, pk=pk)
+   if request.method == "POST":
+       form = InvestmentForm(request.POST, instance=investment)
+       if form.is_valid():
+           investment = form.save()
+           investment.updated_date = timezone.now()
+           investment.save()
+           investments = investment.objects.filter(purchase_date__lte=timezone.now())
+           return render(request, 'portfolio/investment_list.html', {'investments': investments})
+   else:
+       # print("else")
+       form = InvestmentForm(instance=investment)
+   return render(request, 'portfolio/investment_edit.html', {'form': form})
 
+@login_required
+def investment_delete(request, pk):
+   investment = get_object_or_404(Stock, pk=pk)
+   investment.delete()
+   investments = Investment.objects.filter(purchase_date__lte=timezone.now())
+   return render(request, 'portfolio/investment_list.html', {'investments': investments})
+
+@login_required
+def investment_add(request):
+   if request.method == "POST":
+       form = InvestmentForm(request.POST)
+       if form.is_valid():
+           investment = form.save(commit=False)
+           investment.created_date = timezone.now()
+           investment.save()
+           investments = Investment.objects.filter(purchase_date__lte=timezone.now())
+           return render(request, 'portfolio/investment_list.html',
+                         {'investments': investments})
+   else:
+       form = InvestmentForm()
+       # print("Else")
+   return render(request, 'portfolio/investment_add.html', {'form': form})
 
 @login_required
 def portfolio(request,pk):
